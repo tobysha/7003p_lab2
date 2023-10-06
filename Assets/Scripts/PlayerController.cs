cae49a47212af412b5ed6960d83802780fbf8532
tobysha <892715964@qq.com>
2023-10-06 10:39:35 +0100

    new2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem ;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 moveValue ;
+    public float speed ;
+    private int count = 0;
+    private int numPickups = 4;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+    void Start()
+    {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+    void update()
+    {
+        SetCountText();
+    }
+
+    void OnMove( InputValue value ) 
+    {
+        moveValue = value . Get < Vector2 >() ;
+    }
+    
+    
+    void FixedUpdate () 
+    {
+        Vector3 movement = new Vector3 ( moveValue.x, 0.0f , moveValue.y) ;
+        this.GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter ( Collider other)
+    {
+        if(other.gameObject.tag =="pickup")
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score:" + count.ToString();
+        if(count>= numPickups)
+        {
+            winText.text = "YOU WIN?(so easy)";
+        }
+    }
+
+}
